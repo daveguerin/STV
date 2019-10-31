098323ea4a01aad34640812855cd8497e32f46aa
Dave Guerin <dave@dgapps.ie>
2019-10-31 16:51:00 +0000

    UIActivityIndicatorViewStyleMedium as UIActivityIndicatorViewStyleGray is deprecated

--- Sources/STV/SCPullToRefreshView.m
+++ Sources/STV/SCPullToRefreshView.m
@@ -73,7 +73,7 @@
 		_detailTextLabel.textAlignment = NSTextAlignmentCenter;
         [self addSubview:_detailTextLabel];
         
-        _activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+        _activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleMedium];
         _activityIndicator.hidesWhenStopped = TRUE;
         [self addSubview:_activityIndicator];
         
