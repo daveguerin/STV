8ababa60fc8cacf6c85d1749f793974874617658
Dave Guerin <dave@dgapps.ie>
2020-09-14 08:09:33 +0100

    All the H and M files are now back together where they should be! And all in the Sources folder now. Deleted the now not required module.modulemap file.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "STV",
     platforms: [
-      .iOS(.v9)
+        .iOS(.v9)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -23,6 +23,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "STV",
-            dependencies: []),
+            dependencies: [],
+            path: ".",
+            exclude: ["README.md","CHANGELOG.md","DOCUMENTATION.md","License"],
+            sources: ["Sources"],
+            publicHeadersPath: "Sources",
+            cSettings: [.headerSearchPath("Sources")]
+        )
     ]
 )
