98047a6c95dbbd00de5a932b451bc7ccdc870918
Gerald Kandolf <gerald.kandolf@mediatesystems.at>
2021-03-05 09:16:29 +0100

    set detailViewController.modalInPresentation to disable drag to close

--- Sources/SCTableViewSection.m
+++ Sources/SCTableViewSection.m
@@ -2625,6 +2625,11 @@
             
             // TODO: replace this with actual Storyboard segue presentation data
             detailViewController.modalPresentationStyle = UIModalPresentationFormSheet;
+            if (@available(iOS 13.0, *)) {
+                detailViewController.modalInPresentation = YES;
+            } else {
+                // Fallback on earlier versions
+            }
         }
         else
             SCDebugLog(@"Warning: Could not instantiate view controller with id '%@' from Storyboard.", self.ibNewItemViewControllerIdentifier);
